d2a20fdfb9470b0df7ce03fea81a388985689758
Grimmyxiii <bradchundage@gmail.com>
2022-10-24 12:06:19 -0600

    Created a header and navbar for amazonchallenge

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB9iATN48W6usRBYfZoolhvy4zuJUYav68",
+  authDomain: "clone-da02a.firebaseapp.com",
+  projectId: "clone-da02a",
+  storageBucket: "clone-da02a.appspot.com",
+  messagingSenderId: "470045487563",
+  appId: "1:470045487563:web:e8d2144e4d793654f8464e",
+  measurementId: "G-X1WYZX0F9N",
+};
